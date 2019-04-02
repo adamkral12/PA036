a7c8b0a8d219678ea7dcdcc51d2e168fb6e571c8
Marek Valko <valko.marek@gmail.com>
2019-04-02 21:21:17 +0200

    updated schema.sql to import json data

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -1,23 +1,20 @@
 CREATE TABLE emails (
-  id serial NOT NULL,
+  id VARCHAR NOT NULL,
   data json
 );
 
 CREATE TABLE email_events (
-  id serial NOT NULL,
+  id VARCHAR NOT NULL,
   data json
 );
 
 CREATE TABLE emails_with_events (
-  id serial not null,
+  id VARCHAR not null,
   data json
 );
 
-\set content `cat "/data/postgres/emails_with_events.json"`
+\set input `cat "/data/postgres/emails_with_events.json"`
 
-insert into emails_with_events(data) values (json_array_elements(:'content')::json);
+with content as (select * from json_array_elements(:'input'))
 
-
-\set content `cat "/data/postgres/emails_with_events2.json"`
-
-insert into emails_with_events(data) values (json_array_elements(:'content')::json)
+insert into emails_with_events(id,data) select (to_json(c.value)->>'id')::VARCHAR, (to_json(c.value)) from content c
