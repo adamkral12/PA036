36c26875d3b002b29f891a85ad334874a0faad03
Daniel_Vargai <dandi961@gmail.com>
2019-04-09 11:48:22 +0200

    Query test

--- app/main.py
+++ app/main.py
@@ -5,10 +5,13 @@ from app.mongo import MongoDB
 
 def run_postgres_scripts():
     postgres_db = PostgresDb()
-    with open("app/PostgresQueries.json") as json_file:
-        data = json.load(json_file)
-        for p in data:
-            print(p["id"], ":", postgres_db.run_command(postgres_db.execute_all, p["query"]))
+    print(postgres_db.run_command(postgres_db.execute, postgres_db.insert()))
+    print(postgres_db.execute_one("SELECT * FROM emails_with_events WHERE ID = 0;"))
+    # with open("app/PostgresQueries.json") as json_file:
+    #     print(postgres_db.execute_one("SELECT * FROM emails_with_events;"))
+    #     data = json.load(json_file)
+    #     for p in data:
+    #         print(p["id"], ":", postgres_db.run_command(postgres_db.execute_all, p["query"]))
 
 
 def run_mongo_scripts():
--- app/postgres.py
+++ app/postgres.py
@@ -27,9 +27,18 @@ class PostgresDb:
         cur.close()
         return result
 
+    def execute(self, query):
+        cur = self.conn.cursor()
+        cur.execute(query)
+        cur.close()
+
     # should measure the amount of time it took to call `callable`
     # return start, end
     def run_command(self, callable, argument):
         start_time = time.time()
         callable(argument)
         return time.time() - start_time
+
+    def insert(self):
+        element = """{"from":"sureshot-2@earthlink.net","to":"madmike272@hotmail.com","sent":{"date":"2018-08-01 08:00:00.000000","timezone_type":1,"timezone":"+02:00"},"type":"upcoming_journey","events":[{"event_type":"delivery","user_agent":{"browser":null,"platform":null},"position":{"country":null,"city":null,"geographical":{"lat":null,"long":null}},"received":{"date":"2018-08-01 08:04:27.000000","timezone_type":1,"timezone":"+02:00"},"occurred":{"date":"2018-08-01 07:59:59.000000","timezone_type":1,"timezone":"+02:00"},"email_id":"c0b7a14f34d345eda2666d5159c288de"}],"id":"c0b7a14f34d345eda2666d5159c288de"}"""
+        return "INSERT INTO emails_with_events(id, data) VALUES ('0', '{}')".format(element)
