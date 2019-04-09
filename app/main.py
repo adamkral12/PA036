62336a076e72a346f12ff8ae2fae008903ec95f2
Daniel_Vargai <dandi961@gmail.com>
2019-04-09 20:59:30 +0200

    DV_Queries: implemented queries into yaml

--- app/main.py
+++ app/main.py
@@ -24,32 +24,3 @@ if __name__ == '__main__':
         i += 1
 
     print(log_output)
-
-    print("Insert multiple:", postgres_db.run_command(postgres_db.execute, postgres_db.insert_multiple()))
-    print("After 11 insert:",
-          postgres_db.execute_all("SELECT count(*) FROM emails_with_events WHERE (data ->> 'from') = 'test@test.com';"))
-
-    print("Insert single:", postgres_db.run_command(postgres_db.execute, postgres_db.insert_single()))
-    print("After 1 insert:",
-          postgres_db.execute_all("SELECT count(*) FROM emails_with_events WHERE (data ->> 'from') = 'test@test.com';"))
-
-    print("Events before insert:",
-          postgres_db.execute_all("SELECT count(*) FROM (SELECT json_array_elements(data -> 'events') FROM emails_with_events WHERE id = '0') AS events;"))
-    print("Insert event:",
-          postgres_db.run_command(postgres_db.execute, postgres_db.insert_event()))
-    print("Events after insert:",
-          postgres_db.execute_all("SELECT count(*) FROM (SELECT json_array_elements(data -> 'events') FROM emails_with_events WHERE id = '0') AS events;"))
-
-    print("Delete by from:",
-          postgres_db.run_command(postgres_db.execute, postgres_db.delete_by_from()))
-    print("After delete:",
-          postgres_db.execute_all("SELECT count(*) FROM emails_with_events WHERE (data ->> 'from') = 'test@test.com';"))
-
-    print("Delete emails by platform device:",
-          postgres_db.run_command(postgres_db.execute, postgres_db.delete_by_event_device()))
-
-    print("Delete events by platform device:",
-          postgres_db.run_command(postgres_db.execute, postgres_db.delete_event_by_event_device()))
-
-    print("Delete events by event type:",
-          postgres_db.run_command(postgres_db.execute, postgres_db.delete_event_by_event_type()))
