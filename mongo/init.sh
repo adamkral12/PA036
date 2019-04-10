e9674a2dd75e174096863d8e3fa7b4b4b8b8b7c9
Miloš Šilhár <xsilhar@fi.muni.cz>
2019-04-10 19:16:11 +0200

    MongoDB always same initialized docker image

--- mongo/init.sh
+++ mongo/init.sh
@@ -1,1 +1,15 @@
-mongoimport --jsonArray --db $MONGO_INITDB_DATABASE --collection emails --file /data/mongo/emails_with_events.json
+#!/bin/bash
+
+if [ -d ./mongoinit/data ]; then
+  echo "Already initialized."
+  exit 0
+fi
+
+cd mongoinit
+docker build --tag mongoinit:latest .
+init_ID=$(docker run -d -v $(pwd)/data/db:/data/db mongoinit)
+docker logs -f $init_ID | grep -q "init process complete;"
+
+docker stop $(docker ps -f ancestor=mongoinit -q)
+docker rm $(docker ps -a -f ancestor=mongoinit -q)
+docker rmi $(docker images mongoinit -q)
---
+++ mongo/mongoinit/init-database.sh
@@ -0,0 +1,1 @@
+mongoimport --jsonArray --db $MONGO_INITDB_DATABASE --collection emails --file /data/mongo/emails_with_events.json
